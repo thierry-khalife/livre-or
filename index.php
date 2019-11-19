75089656e3272365e07c52e130cab1a81404ef07
Thierry Khalife <thierry.khalife@laplateforme.io>
2019-11-19 11:52:37 +0100

    first commit css

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Livre D'OR</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <nav class="nav">
+            <section class="undernav">
+                <a href="inscription.php"><img src="img/inscription.png"></a>
+                <a href="inscription.php">INSCRIPTION</a>
+            </section>
+            <section class="undernav">
+                <a href="connexion.php"><img src="img/connexion.png"></a>
+                <a href="connexion.php">CONNEXION</a>
+            </section>
+        </nav>
+        <article id="logo">
+            <a href="index.php"><img src="img/or.png"></a>
+        </article>
+        <nav class="nav">
+            <section class="undernav">
+                <a href="admin.php"><img src="img/admin.png"></a>
+                <a href="admin.php">ADMIN PANEL</a>
+            </section>
+            <section class="undernav">
+                <a href="profil.php"><img src="img/profil.png"></a>
+                <a href="profil.php">USER PROFIL</a>
+            </section>
+        </nav>
+    </header>
+
+    <main>
+         <section class="leftsidebar">
+             sqdqsdqsdqsd
+         </section>
+          <section class="rightsidebar">
+             sqdqsdqsdqsd
+         </section>
+    </main>
+
+    <footer>
+        <nav class="navfooter">
+            <a href="inscription.php">INSCRIPTION</a>
+            <a href="connexion.php">CONNEXION</a>
+            <a href="livreor.php">Livre D'or</a>
+            <a href="profil.php">MEMBER PROFIL</a>
+        </nav>
+        <article>
+            <p>Copyright 2019 Coding School | All Rights Reserved | Project by Thierry & Nicolas.</p>
+        </article>
+    </footer>
+
+</body>
+
+</html>
