d22f35c20733aa73ff91be6071b4715232340e3c
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2019-11-21 10:11:29 +0100

    amélioration css index

--- index.php
+++ index.php
@@ -17,14 +17,16 @@
         date_default_timezone_set('Europe/Paris');
         if(isset($_SESSION['login']))
         { 
-            echo "<img src=\"img/perenoelfloss.gif\">";
+            echo "<img class=\"guirlandehaut\" src=\"img/dividerguirlande.png\">";
+            echo "<img id=\"perenoel\" src=\"img/perenoelfloss.gif\">";
             echo "Nous sommes le ".date('d-m-Y')." et il est ".date('H:i:s');
-            echo "<h1>Bonjour ".$_SESSION["login"]."</h1><br>";
+            echo "<h1><img height=35 width=35 src=\"img/candycane.png\">&nbsp Bonjour ".$_SESSION["login"]."&nbsp <img height=35 width=35 src=\"img/snowmanicon.png\"></h1><br>";
             echo "<p>Vous êtes connecté en tant qu'utilisateur. Accédez à votre page de <a href=\"profil.php\">PROFIL</a></p>";
             echo "<p>Ajouter un commentaire dans notre : <a href=\"livre-or.php\">LIVRE D'OR</a></p>";
             echo "<form action=\"index.php\" method=\"post\">
             <input class=\"mybutton\"  name=\"deco\" value=\"Deconnexion\" type=\"submit\" />
             </form>";
+            echo "<img class=\"guirlandebas\" src=\"img/dividerguirlandebas.png\">";
         }
         else
         {
