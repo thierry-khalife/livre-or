8464a862926aebd5022bde5b5511a8523c29c07f
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2019-11-21 17:04:05 +0100

    ajout like dislike

--- livre-or.php
+++ livre-or.php
@@ -4,8 +4,12 @@ $cnx = mysqli_connect("localhost", "root", "", "livreor");
 $requete1 = "SELECT * FROM commentaires";
 $query1 = mysqli_query($cnx, $requete1);
 $resultat = mysqli_fetch_all($query1, MYSQLI_ASSOC);
+var_dump($resultat);
 
-
+$requete2 = "SELECT * FROM utilisateurs WHERE login='".$_SESSION['login']."'";
+$query2 = mysqli_query($cnx, $requete2);
+$resultat2 = mysqli_fetch_all($query2, MYSQLI_ASSOC);
+var_dump($resultat2);
 
 ?>
 
@@ -32,15 +36,62 @@ $resultat = mysqli_fetch_all($query1, MYSQLI_ASSOC);
               $datesql = $resultat[$a]['date'];
               $newdate = date('d-m-Y à H:i:s', strtotime($datesql));
               $iduser = $resultat[$a]['id_utilisateur'];
+              $idmoi = $resultat2[0]['id'];
+              $idcom = $resultat[$a]['id'];
+              $intidmoi = intval($idmoi);
+              $intidcom = intval($idcom);
+              var_dump($intidcom);
               $requetelogin = "SELECT login FROM utilisateurs WHERE id=$iduser";
               $query2 = mysqli_query($cnx, $requetelogin);
               $resultatlogin = mysqli_fetch_all($query2, MYSQLI_ASSOC);
+              $requetecount = "SELECT COUNT(*) FROM votes WHERE id_utilisateur=$intidmoi  AND id_commentaire=$intidcom AND valeur=1";
+              $query3 = mysqli_query($cnx, $requetecount);
+              $resultat3 = mysqli_fetch_all($query3, MYSQLI_ASSOC);
+              $requetecountdislike = "SELECT COUNT(*) FROM votes WHERE id_utilisateur=$intidmoi  AND id_commentaire=$intidcom AND valeur=-1";
+              $query4 = mysqli_query($cnx, $requetecountdislike);
+              $resultat4 = mysqli_fetch_all($query4, MYSQLI_ASSOC);
+              $requetecountlikeall = "SELECT COUNT(*) FROM votes WHERE id_commentaire=$intidcom AND valeur=1";
+              $query5 = mysqli_query($cnx, $requetecountlikeall);
+              $resultat5 = mysqli_fetch_all($query5, MYSQLI_ASSOC);
+              $requetecountdislikeall = "SELECT COUNT(*) FROM votes WHERE id_commentaire=$intidcom AND valeur=-1";
+              $query6 = mysqli_query($cnx, $requetecountdislikeall);
+              $resultat6 = mysqli_fetch_all($query6, MYSQLI_ASSOC);
+              var_dump($resultat3);
+              var_dump($resultat4);
+              var_dump($resultat5);
+              var_dump($resultat6);
               echo "<article id=\"commentaire\">";
               echo "<b><i>Posté le : </i></b>".$newdate;
               echo "<b><i> par : </i></b><u>".$resultatlogin[0]["login"]."</u><br>";
-              echo $resultat[$a]['commentaire']."<br><img id=\"divider\" src=\"img/div.png\">";
+              echo $resultat[$a]['commentaire'];
               echo "<br>";
               echo "</article>";
+              echo "<article id=\"vote\">";
+              echo "<form method=\"post\" action=\"livre-or.php\">";
+              echo "<input type=\"submit\" name=\"likebutton".$a."\" value=\"like\">".$resultat5[0]['COUNT(*)'];
+              echo "<input type=\"submit\" name=\"dislikebutton".$a."\" value=\"dislike\">".$resultat6[0]['COUNT(*)'];
+              echo "</form>";
+              echo "</article>";
+              echo "<img id=\"divider\" src=\"img/div.png\">";
+                if ( isset($_SESSION['login']) && isset($_POST['likebutton'.$a]) && $resultat3[0]['COUNT(*)'] == "0" ) {
+                    if ( $resultat4[0]['COUNT(*)'] == "1" ) {
+                        $requeteresetlike = "DELETE FROM votes WHERE id_commentaire=$intidcom AND id_utilisateur=$intidmoi";
+                        $queryresetlike = mysqli_query($cnx, $requeteresetlike);
+                    }
+                    $requetelike = "INSERT INTO votes (id_utilisateur, id_commentaire, valeur) VALUES ($intidmoi, $intidcom, 1)";
+                    $querylike = mysqli_query($cnx, $requetelike);
+                    $refresh = true;
+                }
+                if ( isset($_SESSION['login']) && isset($_POST['dislikebutton'.$a]) && $resultat4[0]['COUNT(*)'] == "0" ) {
+                    if ( $resultat3[0]['COUNT(*)'] == "1" ) {
+                        $requeteresetdislike = "DELETE FROM votes WHERE id_commentaire=$intidcom AND id_utilisateur=$intidmoi";
+                        $queryresetdislike = mysqli_query($cnx, $requeteresetdislike);
+                    }
+                    $requetedislike = "INSERT INTO votes (id_utilisateur, id_commentaire, valeur) VALUES ($intidmoi, $intidcom, -1)";
+                    $querydislike = mysqli_query($cnx, $requetedislike);
+                    $refresh = true;
+                break;
+                }
               $a++;
           }
           if (!empty($_SESSION['login'])) 
@@ -53,6 +104,9 @@ $resultat = mysqli_fetch_all($query1, MYSQLI_ASSOC);
         <section class="rightsidebar">
        
         <?php
+
+
+
     
         date_default_timezone_set('Europe/Paris');
         if(isset($_SESSION['login']))
